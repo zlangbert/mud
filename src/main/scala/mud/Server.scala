e8df6c915270808f4f91420d77086ac7ced2e6ba
Zach Langbert <zachl44@gmail.com>
2015-12-17 23:13:49 -0600

    Add sbt-revolver

--- src/main/scala/mud/Server.scala
+++ src/main/scala/mud/Server.scala
@@ -2,18 +2,10 @@ package mud
 
 import akka.actor.{ActorSystem, Props}
 
-import scala.io.StdIn
-
-// command parsing
-
 object Server extends App {
 
   implicit val system = ActorSystem()
 
   val server = system.actorOf(Props(new ServerActor(1234, "localhost")), "server")
   server ! ServerActor.Start
-
-  while (StdIn.readLine() != "stop") {}
-  println("Shutting down...")
-  system.terminate()
 }
